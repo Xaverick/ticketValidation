5e9664c634c5395124322b9529be3585ebabfad5
Yash Mittal <mittalyas1234@gmail.com>
2023-12-24 17:54:11 +0530

    email system

--- server/controllers/generalController.js
+++ server/controllers/generalController.js
@@ -1,17 +1,19 @@
 const user = require('../models/userModel');
-const {v4 : uuidv4} = require('uuid')
+const { v4: uuidv4 } = require('uuid')
+const nodemailer = require('nodemailer');
+const { Mailgen } = require('mailgen');
 
 module.exports.generateQRCode = async (req, res) => {
     const { username, email } = req.body;
-    const qr_id = uuidv4()        
+    const qr_id = uuidv4()
 
     const validated = false;
-    if(!username || !email) {
+    if (!username || !email) {
         res.status(400).send('Please enter all fields');
     }
 
     const found = await user.findOne({ email });
-    if(found) {
+    if (found) {
         res.status(400).send('User already exists with this mail');
     }
 
@@ -23,13 +25,13 @@ module.exports.generateQRCode = async (req, res) => {
 
 module.exports.getQRCode = async (req, res) => {
     const { email } = req.body;
-    if(!email) {
+    if (!email) {
         res.status(400).send('Please enter all fields');
     }
 
     const found = await user.findOne({ email });
 
-    if(!found) {
+    if (!found) {
         res.status(400).send('User does not exist');
     }
 
@@ -39,12 +41,12 @@ module.exports.getQRCode = async (req, res) => {
 
 module.exports.validateUser = async (req, res) => {
     const { qr_id } = req.body;
-    if(!qr_id) {
+    if (!qr_id) {
         res.status(400).send('Please enter all fields');
     }
 
     const found = await user.findOne({ qr_id });
-    if(!found) {
+    if (!found) {
         res.status(400).send('User does not exist');
     }
 
@@ -52,5 +54,58 @@ module.exports.validateUser = async (req, res) => {
     await found.save();
     res.json("User validated successfully");
 }
+const transporter = nodemailer.createTransport(
+    {
+        service: 'gmail',
+        auth:
+        {
+            user: 'mittalyas1234@gmail.com',
+            pass: '123456',
+        },
+
+    });
+
+const mailGenerator = new Mailgen(
+    {
+        theme: 'default',
+        product:
+        {
+            name: 'Yash',
+        },
+    });
+const { name, email } = req.body;
+const emailContent = {
+    body: {
+        name,
+        intro: 'welcome to JSCOP',
+        action: {
+            instructions: 'Thank for your registrations . click on button to get your tickect',
+            button:
+            {
+                color: 'red',
+                text: 'tickect for event',
+
+            },
+        },
+        outro: 'Need help,or have questions? Just reply to this email , we\'d love to help',
+    },
+};
+const emailBody = mailGenerator.generate(emailContent);
+const mailOptions = {
+    from: 'mittalyas1234@gmail.com',
+    to: email,
+    subject: 'tickect for event',
+    html: emailBody,
+};
+transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        console.error('Error sending email'.error);
+        res.status(500).send('Internal Server Error');
+    } else {
+        console.log('Email sent:', info.response);
+        res.status(200).send('Registration successful. check your confirmation email and tickect ')
+    }
+});
+
 
 
